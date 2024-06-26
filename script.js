35c57e2e27dbfdda7748cf1397d66730016bcd73
Rafael-12-F <rafifagomes98@gmail.com>
2024-06-26 01:17:16 -0300

    erro no java

---
+++ script.js
@@ -0,0 +1,8 @@
+const convertButton = document.querySelector(".convert-button") 
+
+function convertValues(){
+console.log(" funcionou porra")
+}
+
+convertButton.addEventListener("click",convertValues)
+
