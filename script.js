75130220d82cd4c647e35f1e84f98b93c7a6ddba
Rafael-12-F <rafifagomes98@gmail.com>
2024-07-01 16:42:07 -0300

    automatizaçao feita

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
+
+function convertValues() {
    const inputCurrencyValue = document.querySelector(".input-currency").value
-   console.log("inputCurrencyValue")
 
+   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")/* valor em real*/
+   const currencyValueToConverted = document.querySelector(".currency-value")/* todas outras moedas*/
+
+   console.log(currencySelect.value)
+   const euroToday = 6.3
+   const dollarToday = 5.2
+   const libraToday = 7.10
+
+   if (currencySelect.value == "dollar") {
+      currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+         style: "currency",
+         currency: "USD"
+      }).format(inputCurrencyValue / dollarToday)
+   }
+
+
+   if (currencySelect.value == "euro") {
+      currencyValueToConverted.innerHTML = new Intl.NumberFormat("ed-ED", {
+         style: "currency", currency: "EUR"
+
+      }).format(inputCurrencyValue / euroToday)
+   }
+
+   if (currencySelect.value == "libra") {
+      currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+         style: "currency",
+         currency: "GBP"
+      }).format(inputCurrencyValue / libraToday)
+   }
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+   }).format(inputCurrencyValue)
+
+function changeCurrency(){ 
+   console.log("trocou")
 }
-convertButton.addEventListener("click",convertValues)
+
+currencySelect.addEventListener("change",changeCurrency)
+}
+convertButton.addEventListener("click", convertValues)
